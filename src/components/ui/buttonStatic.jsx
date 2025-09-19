d29fab485509e4f4c55622e706f4cbf7f2039149
helberkaue <helber.avelino05@gmail.com>
2025-09-19 17:16:32 -0300

    Atualizando mockups

--- src/components/ui/buttonStatic.jsx
+++ src/components/ui/buttonStatic.jsx
@@ -3,10 +3,14 @@ import { MdFileDownload } from "react-icons/md";
 const ButtonStatic = () => {
   return (
     <div className="fixed bottom-8 right-8 z-50">
-      <button className="flex items-center gap-2 px-8 py-2 text-white font-semibold rounded-lg bg-gradient-to-r from-blue-400 to-blue-700 hover:from-blue-400  hover:to-blue-600 transition duration-500 text-sm md:text-base shadow-lg">
+      <a
+        href="/curriculo-hk.pdf"
+        download="curriculo-hk.pdf"
+        className="flex items-center gap-2 px-8 py-2 text-white font-semibold rounded-lg bg-gradient-to-r from-blue-400 to-blue-700 hover:from-blue-400 hover:to-blue-600 transition duration-500 text-sm md:text-base shadow-lg"
+      >
         <MdFileDownload size={20} />
         Currículo
-      </button>
+      </a>
     </div>
   );
 };
